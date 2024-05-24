67d20f2d60c5e6fcdbd075079fc0d435ce10f210
MrAlfonzo <s23mactalfr@venta.lv>
2024-05-24 11:59:45 +0300

    Initial

---
+++ src/main/java/lv/latvijasrokdarbi/SparniTimeklaVietneApplication.java
@@ -0,0 +1,13 @@
+package lv.latvijasrokdarbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparniTimeklaVietneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparniTimeklaVietneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/latvijasrokdarbi/SparniTimeklaVietneApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.latvijasrokdarbi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparniTimeklaVietneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
