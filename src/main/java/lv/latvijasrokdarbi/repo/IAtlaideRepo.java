0caa393cd0e121ee1333ec5496b9b059830a9e58
andzekausk <s23kausandz@venta.lv>
2024-06-25 18:48:33 +0300

    Added: Repo files and application.prope

---
+++ src/main/java/lv/latvijasrokdarbi/repo/IAtlaideRepo.java
@@ -0,0 +1,9 @@
+package lv.latvijasrokdarbi.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.latvijasrokdarbi.model.Atlaide;
+
+public interface IAtlaideRepo extends CrudRepository<Atlaide, Integer>{
+
+}
---
+++ src/main/java/lv/latvijasrokdarbi/repo/IPrecesBildeRepo.java
@@ -0,0 +1,9 @@
+package lv.latvijasrokdarbi.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.latvijasrokdarbi.model.PrecesBilde;
+
+public interface IPrecesBildeRepo  extends CrudRepository<PrecesBilde, Integer>{
+
+}
