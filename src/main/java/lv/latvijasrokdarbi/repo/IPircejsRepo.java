6e869d7f07cf5b1921606bf211b437226a0616ce
4fun23 <153755952+4fun23@users.noreply.github.com>
2024-06-26 21:52:32 +0300

    1_26062024
    Added: IPircejsRepo, IPiegadesVeidsRepo, ISamaksasVeidsRepo

---
+++ src/main/java/lv/latvijasrokdarbi/repo/IPircejsRepo.java
@@ -0,0 +1,9 @@
+package lv.latvijasrokdarbi.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.latvijasrokdarbi.model.Pircejs;
+
+public interface IPircejsRepo extends CrudRepository<Pircejs, Integer>{
+
+}
