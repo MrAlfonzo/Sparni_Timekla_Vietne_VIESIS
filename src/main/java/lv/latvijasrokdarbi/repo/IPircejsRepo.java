20e391bb0cb3942f209ee974660020e9d7833d07
4fun23 <153755952+4fun23@users.noreply.github.com>
2024-06-27 13:05:29 +0300

    1_2706024
    Added PirkumaElementsRepo

--- src/main/java/lv/latvijasrokdarbi/repo/IPircejsRepo.java
+++ src/main/java/lv/latvijasrokdarbi/repo/IPircejsRepo.java
@@ -1,9 +1,17 @@
 package lv.latvijasrokdarbi.repo;
 
+import java.util.ArrayList;
+
 import org.springframework.data.repository.CrudRepository;
 
 import lv.latvijasrokdarbi.model.Pircejs;
 
 public interface IPircejsRepo extends CrudRepository<Pircejs, Integer>{
+	
+		Pircejs findByPircejsId(int id);
+		
+		ArrayList<Pircejs> findByEpastsIgnoreCaseContaining(String epasts);
+		
+		ArrayList<Pircejs> findByVardsIgnoreCaseContainingOrUzvardsIgnoreCaseContaining(String vards, String uzvards);
 
 }
